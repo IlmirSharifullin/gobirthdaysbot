50f0d8febceb8ac962844ca288e855d0db54afae
Ilmir <ilmirsharifullin2000@gmail.com>
2024-10-17 20:30:51 +0300

    change birthdays migration

--- db/migrations/20241013183828_new_birthdays_table.sql
+++ db/migrations/20241013183828_new_birthdays_table.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS birthdays(
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name varchar(255) NOT NULL,
     date DATE NOT NULL,
     additional TEXT,
-    user_id BIGINT,
+    user_id BIGINT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 -- +goose StatementEnd

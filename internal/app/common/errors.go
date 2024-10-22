226497720fa469e78b23707795b16d799b19818d
Ilmir <ilmirsharifullin2000@gmail.com>
2024-10-22 23:36:11 +0300

    add callback_data module

--- internal/app/common/errors.go
+++ internal/app/common/errors.go
@@ -5,7 +5,9 @@ import (
 )
 
 var (
-	ErrNoUser            = errors.New("update's user is nil")
-	ErrNotDate           = errors.New("given string is not a date")
-	ErrBirthdayTypeError = errors.New("birthday type is not *storage.Birthday")
+	ErrNoUser                = errors.New("update's user is nil")
+	ErrNotDate               = errors.New("given string is not a date")
+	ErrBirthdayTypeError     = errors.New("birthday type is not *storage.Birthday")
+	ErrCallbackDataNoKey     = errors.New("callback data have no key")
+	ErrCallbackDataWrongType = errors.New("callback data have wrong type value")
 )
